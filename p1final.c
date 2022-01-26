8c115578db1120a55b470b3980e858b732e863b9
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-01-26 14:07:54 +0000

    ..

--- p1final.c
+++ p1final.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
  int main()
-#include<stdio.h>// standard input output
-int main()
  {
   int a,b,c;
-  printf("enter any teo numbers\n");
   printf("enter any two numbers\n");
   scanf("%d%d",&a,&b);
   c=a+b;
